146a4fa70168d497837d0f36881e204eb6730b4b
Felixnite <141184414+Felixnite@users.noreply.github.com>
2023-09-18 04:13:10 +0000

    all the project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
+const values = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+const suits = ["♠", "♣", "♥", "♦"];
+const cardValue = document.querySelector(".card-value");
+const cardSuitTopLeft = document.querySelector(".top-left");
+const cardSuitBottomRight = document.querySelector(".bottom-right");
+const generateBtn = document.querySelector("#generate-btn");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+generateBtn.addEventListener("click", function() {
+  const value = values[Math.floor(Math.random() * values.length)];
+  const suit = suits[Math.floor(Math.random() * suits.length)];
+  cardValue.textContent = value;
+  cardSuitTopLeft.innerHTML = suit;
+  cardSuitBottomRight.innerHTML = suit;
+  cardSuitTopLeft.classList = `card-suit top-left ${suit.toLowerCase()}`;
+  cardSuitBottomRight.classList = `card-suit bottom-right ${suit.toLowerCase()}`;
+});
